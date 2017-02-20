9ab7dec9dba1aa2feb67a9c07f158a7ac55bcfa6
jkmohite96 <jkmohite96@gmail.com>
2017-02-20 12:49:49 +0530

    Named Query example

---
+++ src/main/java/com/journaldev/hibernate/model/Address.java
@@ -0,0 +1,89 @@
+package com.journaldev.hibernate.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+@Table(name = "ADDRESSTable")
+@NamedQueries({ @NamedQuery(name = "@HQL_GET_ALL_ADDRESS", 
+			query = "from Address") })
+@NamedNativeQueries({ @NamedNativeQuery(name = "@SQL_GET_ALL_ADDRESS", 
+			query = "select emp_id, address_line1, city, zipcode from Address") })
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	private long id;
+
+	@Column(name = "address_line1")
+	private String addressLine1;
+
+	@Column(name = "zipcode")
+	private String zipcode;
+
+	@Column(name = "city")
+	private String city;
+
+	@OneToOne(cascade=CascadeType.ALL,mappedBy="address")
+	private Employee employee;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressLine1= " + addressLine1 + ", City=" + city
+				+ ", Zipcode=" + zipcode;
+	}
+}
---
+++ src/main/java/com/journaldev/hibernate/model/Employee.java
@@ -0,0 +1,66 @@
+package com.journaldev.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMPLOYEETable")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	private long id;
+
+	@Column(name = "emp_name")
+	private String name;
+
+	@Column(name = "emp_salary")
+	private double salary;
+
+	@OneToOne
+	private Address address;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Id= " + id + ", Name= " + name + ", Salary= " + salary
+				+ ", {Address= " + address + "}";
+	}
+
+}
